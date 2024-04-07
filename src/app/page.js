ed8bb7daf4480726e578dfc67f36aadd81f5f517
Md Serif <mdserif1998@gmail.com>
2024-04-07 19:31:43 +0530

    Completed the UI Part

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,19 @@
-import Image from "next/image";
+"use client"
+
+import { useState,useEffect } from "react";
 
 export default function Home() {
   return (
-    <main>
-      <h1>hello world</h1>
+    <main className="flex flex-col justify-center items-center w-screen h-screen p-24 bg-slate-500">
+      <div className="w-full h-1/2 flex flex-col gap-6 justify-center items-center bg-red-400 rounded-2xl">
+
+        <h2 className="font-bold text-3xl">Select Country and State of Your Choice</h2>
+        <select name="Country" id="_1" place className="border-solid border-black border-[2px]  w-1/2 rounded-xl p-1 "><option value="" className="text-center">--Country--</option></select>
+        <select name="Country" id="_1" className="border-[2px] border-solid border-black w-1/2 rounded-xl p-1"><option value="" className="text-center">--State--</option></select>
+
+      </div>
     </main>
   );
 }
+
+
